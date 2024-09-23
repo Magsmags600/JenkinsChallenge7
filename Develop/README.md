be9b581f213878dc94357f369b8eb2a9a7f73978
Magsmags600 <margaretjenkins@gmail.com>
2024-09-22 18:26:33 -0700

    firstdraft

---
+++ Develop/README.md
@@ -0,0 +1,36 @@
+
+# Professional Readme.md File
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+Create a professional README file generator so that the file will be generated and more time is spent on coding other parts of the application. 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Run index.js in the terminal. 
+
+## Usage
+This README generator can be used to create a professional README file. 
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+Just me for now. 
+
+## Tests
+Not to sure how to test yet. Change this question bro. 
+
+## Questions
+If you have any questions, feel free to contact me:
+
+- GitHub: [magsmags600](https://github.com/magsmags600)
+- Email: margaretjenkins@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,137 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
+// // Function call to initialize app
+// init();
+import fs from 'fs';
+import inquirer from 'inquirer'
+
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Describe the User Story:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Review professional readme doc and put another question here?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:'
+  }
+];
+
+// Function to generate the license badge based on user's choice
+function generateLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;
+  }
+  return '';
+}
+
+// Function to generate the README file content
+function generateReadMe(answers) {
+  return `
+# ${answers.title}
+
+${generateLicenseBadge(answers.license)}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+If you have any questions, feel free to contact me:
+
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+`;
+}
+
+// Function to initialize the app
+function init() {
+  inquirer.prompt(questions)
+    .then(answers => {
+      const readmeContent = generateReadMe(answers);
+
+      // Write README file
+      fs.writeFile('README2.md', readmeContent, (err) => {
+        if (err) {
+          console.error('Error writing file:', err);
+        } else {
+          console.log('Successfully generated README.md');
+        }
+      });
+    })
+    .catch((error) => {
+      console.error('Error during prompting:', error);
+    });
+}
+
+// Call the init function to start the application
 init();
